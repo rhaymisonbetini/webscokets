4bbfd300b99f2fa9a2795a26f5b875bda3e4cb85
RHAYMISON <rhaymison@hotmail.com>
2021-12-22 17:50:44 -0300

    initial commit

---
+++ chat/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const socketio = require('socket.io');
+
+app.use(express.static(__dirname + '/public'))
+
+const expressServer = app.listen(9000);
+const io = socketio(expressServer)
+
+io.on('connection', (socket) => {
+
+    socket.emit('messageFromServer', { data: "Wellcome to the jungle!" })
+
+    socket.on("messageToServer", (msg) => {
+        console.log(msg)
+    })
+})
+
+
